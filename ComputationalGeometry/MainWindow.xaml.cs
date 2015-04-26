a877a4554498ca529ed68a86c27a98744273e8fd
isvb <i.burdukov@gmail.com>
2015-04-26 18:09:45 +0600

    Menu created

--- ComputationalGeometry/MainWindow.xaml.cs
+++ ComputationalGeometry/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DColor = System.Drawing.Color;
 
 namespace ComputationalGeometry
 {
@@ -23,6 +26,26 @@ namespace ComputationalGeometry
         public MainWindow()
         {
             InitializeComponent();
+            var bmp = new Bitmap(100, 100);
+            for (int i = 0; i < 10; ++i)
+                for (int j = 0; j < 10; ++j)
+                    bmp.SetPixel(i, j, DColor.Aqua);
+            image.Source = BitmapToImageSource(bmp);
+        }
+
+        public BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                return bitmapimage;
+            }
         }
     }
 }
