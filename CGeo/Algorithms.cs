4283d4a59e9a1e4a82d905665aef6a49f1899e1a
isvb <i.burdukov@gmail.com>
2015-04-26 20:47:57 +0600

    Defined data structure

---
+++ CGeo/Algorithms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGeo
+{
+    public static class Algorithms
+    {
+        public static Triangle[] CreateSuperstructure(Point topLeft, Point bottomRight)
+        {
+            // Initial triangles.
+            var left = new Triangle();
+            var right = new Triangle();
+            // Define another two points of rectangle with diagonal defined by points topLeft & bottomRight.
+            var bottomLeft = new Point(topLeft.X, bottomRight.Y);
+            var topRight = new Point(bottomRight.X, topLeft.Y);                        
+            // Define ribs of superstructure.
+            var diagonal = new Rib(topLeft, bottomRight, left, right);
+            var leftRib = new Rib(topLeft, bottomLeft, left, null);
+            var rightRib = new Rib(topRight, bottomRight, right, null);
+            var topRib = new Rib(topLeft, topRight, right, null);
+            var bottomRib = new Rib(bottomLeft, bottomRight, left, null);                       
+            // Set ribs for triangles.
+            left.SetRibs(leftRib, bottomRib, diagonal);
+            right.SetRibs(rightRib, topRib, diagonal);
+            
+            return new Triangle[] { left, right };            
+        }
+    }
+}
---
+++ CGeo/DataStructures.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGeo
+{
+    public struct Point
+    {
+        public double X;
+
+        public double Y;
+        
+        public Point(double X, double Y) : this() 
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+
+    public class Rib
+    {
+        public Point A;
+        public Point B;
+
+        public Triangle T1;
+        public Triangle T2;
+
+        public IEnumerable<Point> Points { get { yield return A; yield return B; } }
+        public IEnumerable<Triangle> Triangles { get { yield return T1; yield return T2; } }
+
+        public Rib() { }
+
+        public Rib(Point A, Point B, Triangle T1, Triangle T2)
+        {
+            this.A = A;
+            this.B = B;
+            this.T1 = T1;
+            this.T2 = T2;
+        }
+    }
+
+    public class Triangle
+    {
+        public Rib R1;
+        public Rib R2;
+        public Rib R3;
+
+        public IEnumerable<Rib> Ribs { get { yield return R1; yield return R2; yield return R3; } }
+
+        public void SetRibs(Rib R1, Rib R2, Rib R3)
+        {
+            this.R1 = R1;
+            this.R2 = R2;
+            this.R3 = R3;
+        }
+    }
+}
