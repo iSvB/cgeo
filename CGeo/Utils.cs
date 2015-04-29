d5a4f68b227883cfa61a5791592afed117739835
iSvB <i.burdukov@gmail.com>
2015-04-29 10:28:28 +0600

    Implemented calculations of Z-component of cross product

---
+++ CGeo/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGeo
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Computes z-component of cross product of vectors [a,b] in right-handed coordinate system.        
+        /// </summary>
+        /// <param name="aStart">Initial point of vector a.</param>
+        /// <param name="aEnd">Terminal point of vector a.</param>
+        /// <param name="bStart">Initial point of vector b.</param>
+        /// <param name="bEnd">Terminal point of vector b.</param>
+        /// <returns>Z-component of cross product.</returns>        
+        public static double CrossProductZ(Point aStart, Point aEnd, Point bStart, Point bEnd)
+        {
+            return (aEnd.X - aStart.X) * (bEnd.Y - bStart.Y) - (aEnd.Y - aStart.Y) * (bEnd.X - bStart.X);
+        }
+    }
+}
---
+++ CGeoTest/UtilsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CGeo;
+
+namespace CGeoTest
+{
+    [TestClass]
+    public class UtilsTest
+    {
+        [TestMethod]
+        public void CrossProductZ()
+        {
+            // Arrange
+            var A = new Point(2, 0);
+            var B = new Point(3, 2);
+            var C = new Point(1, 0);
+            var D = new Point(0, 2);            
+            // Act
+            double ab_z = Utils.CrossProductZ(A, B, C, D);
+            double ba_z = Utils.CrossProductZ(C, D, A, B);
+            // Assert
+            Assert.AreEqual(4, ab_z);
+            Assert.AreEqual(-4, ba_z);
+        }
+    }
+}
